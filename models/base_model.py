8bc07cc97f22ca898773ee487a173a6f6f4ff4e7
Agure-la <lameckagure@gmail.com>
2023-10-12 02:39:18 -0700

    completed task 5

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel():
     """ defines common attributes and methods in other classes"""
@@ -27,6 +28,7 @@ class BaseModel():
             self.id =str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """ returns stringrepresentation of the object """
@@ -35,6 +37,7 @@ class BaseModel():
     def save(self):
         """ updates updated time with current time"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ returns dictionary representation of the object"""
